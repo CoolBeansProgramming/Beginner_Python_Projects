1fcb373d4168bdae949e908b67c2d08575198db5
Your Name <you@example.com>
2020-05-20 17:09:01 -0400

    Added welcome screen

---
+++ Dice_Rolling_Simulator.py
@@ -0,0 +1,7 @@
+# Dice Rolling Simulator
+
+print("Welcome to Dice Rolling Simulator")
+print("Which type of die would you like to use?")
+print("The options are four, size, eight, and ten sided dice.")
+size = input("Enter the numeric value: ")
+print(size)
