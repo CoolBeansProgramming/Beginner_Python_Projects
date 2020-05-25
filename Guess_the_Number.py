847e4e0cb26e815e4550370cae233cbe0665181a
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-05-24 20:51:49 -0400

    Started gues the number program

---
+++ Guess_the_Number.py
@@ -0,0 +1,8 @@
+# Guess the number
+
+import random
+
+print("Hello and welcome to my number guessing game!")
+number = input("Please choose a number between 1 and 100.")
+
+rand = random.randint(1, 100)
