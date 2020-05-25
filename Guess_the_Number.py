8e6b48a6d61473194fac0ab682742359f3317aaf
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-05-24 21:14:21 -0400

    Added rough draft of code

--- Guess_the_Number.py
+++ Guess_the_Number.py
@@ -5,4 +5,14 @@ import random
 print("Hello and welcome to my number guessing game!")
 number = input("Please choose a number between 1 and 100.")
 
-rand = random.randint(1, 100)
+loop = True
+while loop:
+    rand = random.randint(1, 100)
+
+    if number < rand:
+        print("You guessed too low!")
+    elif number > rand:
+        print("You guessed too high!")
+    break
+
+print("You guessed correctly! My number was", rand)
