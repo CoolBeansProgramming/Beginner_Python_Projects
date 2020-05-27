0df7c1d5e2b4ece8d952eb0b835452d4cc2a5e0c
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-05-26 21:02:01 -0400

    Started mad libs, added story title

---
+++ Mad_Libs_Generator.py
@@ -0,0 +1,8 @@
+# Mad Libs Generator
+
+
+print("Welcome to Mad Libs Generator")
+print('Our short story is: "This Morning" ')
+
+color = input("Enter a color: ")
+animal =input("Enter an animal: ")
