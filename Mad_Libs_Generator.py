b0232f82548ff6add626044465f1495a686d3cec
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-05-27 20:10:02 -0400

    initial story

--- Mad_Libs_Generator.py
+++ Mad_Libs_Generator.py
@@ -7,9 +7,15 @@ print('Our short story is: "This Morning" ')
 animal =input("Enter an animal: ")
 color = input("Enter a color: ")
 noise =input("Enter a sound word: ")
+food =input("Enter a food item: ")
+noun1 = input("Enter a noun: ")
+adverb = input("Enter an adverb: ")
+place = input("Enter a place: ")
+
 
 
 print("Today I woke up to the sound of a", animal, "coming from my stomach! I was quite surprised.")
 print("Never the less, I got up to began to get ready for my day.")
 print("Upon entering the bathroom and casting in glance in the mirror, I noticed my skin" ,\
       "had turned a lovely shade of", color)
+print("Anyways, I brushed my teeth with my", noun1, "and,", adverb, "got dressed for my morning run through", place)
