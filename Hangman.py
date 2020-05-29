3d4595db712e0032748545829dd0f6358e52487d
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-05-29 18:34:02 -0400

    reading in easy text file into a list

--- Hangman.py
+++ Hangman.py
@@ -3,3 +3,13 @@
 print("Welcome to hangman!")
 # print("Choose your level: easy, medium, hard.")
 
+easy_file = open("easy_words.txt", 'r')
+easy = easy_file.readlines()
+
+newList = []
+for line in easy:
+    if line[-1] == '\n':
+        newList.append(line[:-1])
+    else:
+        newList.append(line)
+print(newList)
