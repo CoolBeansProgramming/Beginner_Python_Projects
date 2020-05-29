6382798b2c4d945f6df95850c9c707e1959439cc
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-05-29 18:10:42 -0400

    wording

--- Mad_Libs_Generator.py
+++ Mad_Libs_Generator.py
@@ -8,7 +8,7 @@ animal =input("Enter an animal: ")
 color = input("Enter a color: ")
 noise =input("Enter a sound word: ")
 food =input("Enter a food item: ")
-noun1 = input("Enter a noun: ")
+noun = input("Enter a noun: ")
 adverb = input("Enter an adverb: ")
 place = input("Enter a place: ")
 
@@ -18,4 +18,4 @@ print("Today I woke up to the sound of a", animal, "coming from my stomach! I wa
 print("Never the less, I got up to began to get ready for my day.")
 print("Upon entering the bathroom and casting in glance in the mirror, I noticed my skin" ,\
       "had turned a lovely shade of", color)
-print("Anyways, I brushed my teeth with my", noun1, "and,", adverb, "got dressed for my morning run through", place)
+print("Anyways, I brushed my teeth with my", noun, "and,", adverb, "got dressed for my morning run through", place)
