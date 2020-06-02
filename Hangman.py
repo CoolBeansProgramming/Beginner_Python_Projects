e2e4ac74d9cc091711cf04e489f67a43bc34acb6
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-06-02 19:51:54 -0400

    added randomized word selection

--- Hangman.py
+++ Hangman.py
@@ -2,34 +2,39 @@
 
 import random
 
-def game():
+def game(f):
     newList = []
     for line in f:
         if line[-1] == '\n':
             newList.append(line[:-1])
         else:
             newList.append(line)
-    print(random.randint(1,len(newList)))
-    print(len(newList))
-    g =input("Make your first guess: ")
+    word = random.choice(newList)
+    print(word)
+    guess = input("Make your first guess: ")
+    if guess in word:
+        print("Good guess! The letter", guess, "is in the word.")
+    else:
+        print("The letter", guess, "is not in the word.")
 
 print("Welcome to hangman!")
 level =input("Choose your level: easy, medium, hard. ")
 
+
 if level == 'easy':
     file = open("easy_words.txt", 'r')
     f = file.readlines()
-    game()
+    game(f)
 
 elif level == 'medium':
     file = open("medium_words.txt", 'r')
     f = file.readlines()
-    game()
+    game(f)
 
 elif level == 'hard':
     file = open("hard_words.txt", 'r')
     f = file.readlines()
-    game()
+    game(f)
 
 else:
     print("Please select easy, medium, or hard. ")
