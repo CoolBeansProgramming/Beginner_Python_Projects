f76d6b1f82dd173f0a3f7ce878fc8560fecf1885
Mona Lisa <50372589+CoolBeansProgramming@users.noreply.github.com>
2020-06-02 19:36:12 -0400

    created one game function to avoid repetition

--- Hangman.py
+++ Hangman.py
@@ -1,48 +1,36 @@
 # Hangman
 
-def easy():
-    easy_file = open("easy_words.txt", 'r')
-    easy = easy_file.readlines()
+import random
 
+def game():
     newList = []
-    for line in easy:
+    for line in f:
         if line[-1] == '\n':
             newList.append(line[:-1])
         else:
             newList.append(line)
-    print(newList)
-
-def medium():
-    easy_file = open("easy_words.txt", 'r')
-    easy = easy_file.readlines()
-
-    newList = []
-    for line in easy:
-        if line[-1] == '\n':
-            newList.append(line[:-1])
-        else:
-            newList.append(line)
-        print(newList)
-
-def hard():
-    easy_file = open("easy_words.txt", 'r')
-    easy = easy_file.readlines()
-
-    newList = []
-    for line in easy:
-        if line[-1] == '\n':
-            newList.append(line[:-1])
-        else:
-            newList.append(line)
-        print(newList)
+    print(random.randint(1,len(newList)))
+    print(len(newList))
+    g =input("Make your first guess: ")
 
 print("Welcome to hangman!")
 level =input("Choose your level: easy, medium, hard. ")
 
 if level == 'easy':
-    easy()
+    file = open("easy_words.txt", 'r')
+    f = file.readlines()
+    game()
+
 elif level == 'medium':
-    medium()
+    file = open("medium_words.txt", 'r')
+    f = file.readlines()
+    game()
+
 elif level == 'hard':
-    hard()
+    file = open("hard_words.txt", 'r')
+    f = file.readlines()
+    game()
+
+else:
+    print("Please select easy, medium, or hard. ")
 
